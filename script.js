4dd7b4883d041acacc37c580029cc0584b4df76c
Chupacabriks <mufunbetsuna@gmail.com>
2025-02-17 16:42:21 +0300

    второй урок

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const a = 1;
-let b = "вторая переменная";
-let title = 0;
-let screens = 0;
-let screenPrice = 0;
-let rollback = 0;
-let fullPrice = 0;
-let adaptive = 0;
-alert("Первый урок по js");
-console.log("JS");
+// Присвоить значения
+let title = 'Урок02';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 7;
+let rollback = 72;
+let fullPrice = 999999;
+let adaptive = false;
+
+// Вывести в консоль
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+const stre = "Стоимость верстки экранов" + " " + screenPrice + " " + "миллионов долларов";
+console.log(stre);
+const strs = "Стоимость разработки сайтов " + " " + fullPrice + " " + "долларов";
+console.log(strs);
+
+// нижний регистр 
+console.log(screens.toLowerCase());
+
+//массив
+console.log(screens.split());
+
+//процент отката
+let perOfRoll = (fullPrice * (rollback / 100));
+console.log('Процент отката:' + ' ' + perOfRoll);
