b186967558e1b1cd51c20e90caaa51e80f412b5e
Chupacabriks <mufunbetsuna@gmail.com>
2025-03-24 21:04:05 +0300

    урок 5. не могу посчитать итоговую стоимость и скидку при пустом значении

--- script.js
+++ script.js
@@ -1,53 +1,79 @@
 'use strict';
 //БЛОК ОБЪЯВЛЕНИЯ
 // Присвоить значения 
-let title = prompt('Как называется Ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?');
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
-
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let fullPrice;
 let allServicePrices;
 let servicePercentPrice;
+let service1;
+let service2;
 
-const rollback = 72;
-// Вычислить итоговую стоимость работы 
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const rollback = 12;
 
-const rebatePercentage = (fullPrice * (rollback / 100));
-
-
-const stre = "Стоимость верстки экранов" + " " + screenPrice + " " + "миллионов долларов";
-const strs = "Стоимость разработки сайтов " + " " + fullPrice + " " + "долларов";
+//БЛОК ОПИСАНИЯ ФУНКЦИЙ
 
 
 
-//БЛОК ОПИСАНИЯ ФУНКЦИЙ 
-const getAllServicePrices = function (servicePrice1, servicePrice2) {
-  return servicePrice1 + servicePrice2;
-}
+//обозначаем эти функции 
 
-
-//functionDecloration 
-function getFullPrice(screenPrice, allServicePrices) {
-  return screenPrice + allServicePrices;
-}
-
-const getTitle = function (title) {
+//название 
+const getTitle = function () {
   let string = title.trim().toLowerCase();
   return (title = string.charAt(0).toUpperCase() + string.slice(1));
-}
+};
 
-const getServicePercentPrices = function (fullPrice, rebatePercentage) {
-  return Math.ceil(fullPrice - rebatePercentage);
-}
+const getScreenPrice = function () {
+  do {
+    screenPrice = prompt('Сколько будет стоить данная работа?');
+    if (screenPrice !== null) {
+      screenPrice = screenPrice.trim();
+    }
+  } while (!isNumber(screenPrice));
+};
+
+const getAllServicePrices = function () {
+  let sum = 0;
+  for (let i = 0; i < 2; i++) {
+    let price;
+    if (i === 0) {
+      service1 = prompt('Какой дополнительный тип услуги нужен?');
+    } else if (i === 1) {
+      service2 = prompt('Какой дополнительный тип услуги нужен?');
+    }
+    do {
+      price = prompt('Сколько это будет стоить?');
+      if (price === null || price.trim() === '') {
+        price = 0;
+        break;
+      }
+      price = price.trim();
+    } while (!isNumber(price));
+    sum += +price;
+  }
+  return sum;
+};
+
+
+//проверка числа
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+//functionDecloration
+const getFullPrice = function () {
+  return Number(screenPrice) + Number(allServicePrices);
+};
+
+const getServicePercentPrices = function () {
+  return fullPrice * (1 - rollback / 100);
+};
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
-}
+};
 
 const getRollBackMessage = function (price) {
   if (price > 30000) {
@@ -59,21 +85,32 @@ const getRollBackMessage = function (price) {
   } else if (price < 0) {
     return 'Что-то пошло не так'
   }
-}
+};
+
+//какие функции для вопросов пользователю надо вызвать: 
+const asking = function () {
+  title = prompt('Как называется Ваш проект?');
+  screens = prompt('Какие типы экранов нужно разработать?', 'Простые и сложные');
+  screenPrice = getScreenPrice();
+  adaptive = confirm('Нужен ли адаптив на сайте?');
+  allServicePrices = getAllServicePrices();
+};
 
 //БЛОК ФУНКЦИОНАЛА
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-fullPrice = getFullPrice(screenPrice, allServicePrices);
-title = getTitle(title);
-servicePercentPrice = getServicePercentPrices(fullPrice, rebatePercentage);
+asking();
+title = getTitle();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
 
-//БЛОК ВЫВОДА
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
-//вызовы typeof
 
-console.log(screens.split()); //типы экранов //массив
+// БЛОК ВЫЗОВА
+console.log('Стоимость всех доп.услуг:', allServicePrices); //стоимость всех доп услуг
 console.log(getRollBackMessage(fullPrice)); //скидка пользователю 
-console.log('Итоговая стоимость: ' + servicePercentPrice);//итоговая стоимость
-
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+console.log('Типы экранов:' + screens.split()); //типы экранов
+console.log('Итоговая стоимость: ' + servicePercentPrice.toFixed(2));//итоговая стоимость
